e279d9cbc1ec1dc22e4be915a07d1283967ed6a8
stealth1018 <stealth1018@gmail.com>
2017-08-08 16:05:58 +0900

    pid 변경

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -317,14 +317,14 @@
 
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
   // Ultimaker
-//  #define  DEFAULT_Kp 22.2
-//  #define  DEFAULT_Ki 1.08
-//  #define  DEFAULT_Kd 114
+  #define  DEFAULT_Kp 22.2
+  #define  DEFAULT_Ki 1.08
+  #define  DEFAULT_Kd 114
 
 		// Ultimaker2
-    #define  DEFAULT_Kp 9.12
-    #define  DEFAULT_Ki 0.41
-    #define  DEFAULT_Kd 50.98
+    //#define  DEFAULT_Kp 9.12
+    //#define  DEFAULT_Ki 0.41
+    //#define  DEFAULT_Kd 50.98
 
   // MakerGear
   //#define  DEFAULT_Kp 7.0
@@ -366,14 +366,14 @@
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-//  #define  DEFAULT_bedKp 10.00
-//  #define  DEFAULT_bedKi .023
-//  #define  DEFAULT_bedKd 305.4
+  #define  DEFAULT_bedKp 10.00
+  #define  DEFAULT_bedKi .023
+  #define  DEFAULT_bedKd 305.4
 
   //Ultimaker2
-  #define  DEFAULT_bedKp 124.55
-  #define  DEFAULT_bedKi 23.46
-  #define  DEFAULT_bedKd 165.29
+//  #define  DEFAULT_bedKp 124.55
+//  #define  DEFAULT_bedKi 23.46
+//  #define  DEFAULT_bedKd 165.29
 
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
